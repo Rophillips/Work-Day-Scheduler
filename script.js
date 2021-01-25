3c60fc4315e036201fb541a1597fd062b7520565
Roseanne Phillips <rphillips.rp@gmail.com>
2021-01-25 17:23:03 -0500

    changes for local storage

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ var twelve = document.querySelector("#twelvePM");
 var one = document.querySelector("#onePM");
 var two = document.querySelector("#twoPM");
 var three = document.querySelector("#threePM");
-var four= document.querySelector("#fourPM");
+var four = document.querySelector("#fourPM");
 var five = document.querySelector("#fivePM");
 
 
+console.log(JSON.parse(localStorage.getItem("schedule")))
 var currentDate = $("#currentDay");
 
 //days array
@@ -31,44 +32,75 @@ var date = currentDay.getDate();
 var year = new Date().getYear()
 var currentTime = new Date().getHours();
 
-
-
 var timeBlock = $(".time-block");
 
-var timeBlockValue = parseInt($(timeBlock[i]).attr("value"));
-
-for(var i = 0; i < timeBlock.length; i++){
-    var timeBlockValue = parseInt($(timeBlock[i]).attr("value"));
-    if (timeBlockValue < currentTime) {
-        $(timeBlock[i]).addClass("past");
-
-    }else if(timeBlockValue === currentTime){
-        $(timeBlock[i]).addClass("present");
-        
-    }else if (timeBlockValue > currentTime){
-        $(timeBlock[i]).addClass("future");
-    }
-}
+//var timeBlockValue = parseInt($(timeBlock[i]).attr("value"));
 
 //if(i === 12){
-  //timeBlock.css(["background-color", "red"]);
-  //timeBlock.addClass("red-class");
+//timeBlock.css(["background-color", "red"]);
+//timeBlock.addClass("red-class");
 //}
 //if((new Date()).getHours() === i){ 
-  //timeBlock.addClass("red-class");
+//timeBlock.addClass("red-class");
 //}
 //push local storage
 var userInput = [];
 
 
-$(".saveBtn").on("click", function(){
-  $(".task").each(function (i){
+function add_blocks_dynamically() {
+  let locally_stored_schedule = JSON.parse(localStorage.getItem("schedule"))
+  for (var i = 9; i <= 17; i++) {
+    // If time is past, present or future
+    let time_class = ""
+    if (i < currentTime) {
+      time_class = "past"
+    } else if (i === currentTime) {
+      time_class = "present"
+    } else if (i > currentTime) {
+      time_class = "future"
+    }
+
+    // If I is past noon
+    if (i <= 12) {
+      time = i
+      half = "am"
+    } else {
+      time = i - 12
+      half = "pm"
+    }
+
+    if (locally_stored_schedule[`input${i}`] !== "undefined") {
+      stored = locally_stored_schedule[`input${i}`]
+    } else {
+      stored = ""
+    }
+
+
+    $(".container").append(`<div class="row my-row">
+  <div class="col-1 my-col col-b1 hour" id="elevenAM">${time} ${half}</div>
+  <div class="col-9 my-col col-b2 time-block" value="11">
+    <textarea id="input${i}" class="task ${time_class}">${stored}</textarea>
+</div>
+  <button class="col-1 my-col col-b3 fa saveBtn"></button>
+</div>`)
+  }
+}
+
+
+add_blocks_dynamically()
+
+$(".saveBtn").on("click", function () {
+  let schedule = {}
+  $(".task").each(function (i) {
     var text = $(this).val();
     console.log(this);
 
     var time = $(this).attr("id");
     console.log(time, text);
-    localStorage.setItem(time, text);
+    schedule[time] = text
+    ///schedule.push({"input13":time})
   })
+
+  localStorage.setItem("schedule", JSON.stringify(schedule))
 
 })
